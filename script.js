96f88c94528ec03e11a02b3251e8c37b9a95a8c5
Jeremy <jerywarne7@gmail.com>
2022-04-30 10:06:23 +0300

    added functionality on side navbar features and company links

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const menuIcon = document.querySelector(".navbar-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const sideNavBar = document.querySelector("#sideNavbar");
 const overlay = document.querySelector(".overlay");
+const sideCompanyTab = document.querySelector("#side-company");
+const sideFeaturesTab = document.querySelector("#side-features");
 
 //open side navbar
 menuIcon.addEventListener("click", openSideNavbar);
@@ -28,9 +30,18 @@ function closeSideNavbar() {
 }
 
 /* *****************************************************
-hovering over the navbar
+side navbar dropdowns 
 ************************************************/
-const dropdownMenus = document.querySelectorAll(".dropdown-menu");
-console.log(dropdownMenus);
+sideFeaturesTab.addEventListener("click", showTab);
+sideCompanyTab.addEventListener("click", showTab2);
 
-//hover event
+//function show tab
+function showTab() {
+  const sideFeatures = sideFeaturesTab.nextElementSibling;
+  sideFeatures.classList.toggle("hide");
+}
+
+function showTab2() {
+  const sideCompany = sideCompanyTab.nextElementSibling;
+  sideCompany.classList.toggle("hide");
+}
